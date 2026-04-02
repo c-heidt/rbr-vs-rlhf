33ff618ac19b522c11b71883574f14d5cc59d114
c-heidt <caroheidt99@gmail.com>
2026-04-02 13:35:30 +0200

    fix project dir and install in sft.sh

--- sft.sh
+++ sft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=llm-sft
-#SBATCH --output=logs/cluster-%j.out
-#SBATCH --error=logs/cluster-%j_error.out
+#SBATCH --output=logs/lm_sft-%j.out
+#SBATCH --error=logs/lm_sft-%j_error.out
 #SBATCH --partition=gpu_h100
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
@@ -12,7 +12,7 @@
 #SBATCH --mail-type=ALL
 
 
-PROJECT_DIR=$(dirname "$(realpath "$0")")
+PROJECT_DIR=$($SLURM_SUBMIT_DIR)
 CONDA_ENV=${CONDA_ENV:-"base"}
 
 echo "Activating conda environment: $CONDA_ENV"
@@ -21,7 +21,7 @@ conda activate $CONDA_ENV
 
 echo "Load cuda module and install flash attention"
 module load devel/cuda/12.8
-pip install flash-attn @ https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.5cxx11abiFALSE-cp312-cp312-linux_x86_64.whl
+pip install flash-attn@https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.5cxx11abiFALSE-cp312-cp312-linux_x86_64.whl
 
 echo "Running the SFT script"
 python "$PROJECT_DIR/sft.py" --project_dir "$PROJECT_DIR"
